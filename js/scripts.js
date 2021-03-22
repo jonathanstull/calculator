98bca9c50714b73711424d66a822b427c59f72f2
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-03-22 13:12:06 -0700

    insert additional functions for subtract and multiply

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,35 @@
-tfunction add(number1, number2) {
+function add(number1, number2) {
   return number1 + number2;
-  }
-  
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another number:"));
-  
-  alert(add(number1, number2)); 
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const result = add(number1, number2);
+
+alert(result);
+
+function add(number1, number2) {
+	return number1 + number2;
+}
+
+function subtract(number1, number2) {
+	return number1 + " - " + number2 + " = " + (number1 - number2);
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const difference = subtract(number1, number2);
+
+alert(difference);
+
+function multiply(number3, number4) {
+	return number3 + " * " + number4 + " = " + number3 * number4;
+}
+
+const number3 = parseInt(prompt("Enter a number:"));
+const number4 = parseInt(prompt("Enter another number:"));
+const product = multiply(number3, number4);
+
+alert(product);
+
+const result = add(number1, number2);
